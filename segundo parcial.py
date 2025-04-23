5e50b8c22cb723299627ee9ca019ee48ddf9e0a6
IsabellaAgudelo01 <isabella.agudelo01@usa.edu.co>
2025-04-23 08:59:43 -0500

    Create segundo parcial.py

---
+++ segundo parcial.py
@@ -0,0 +1,11 @@
+```
+for j in range()
+    a = int(input('Número total de productos a registrar: '))
+        for i in range (a):
+        b = int(input('Ingrese cantidad en inventario de cada producto: '))
+lista = [b][a]
+while (b<10):
+    
+    
+no pude
+```
